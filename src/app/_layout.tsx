ee1026bf28c540dc2dcc99724950761919ec9e9d
Sbtech24 <bajomosemilore@gmail.com>
2026-04-11 04:11:08 +0100

    initial commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,37 @@
+import { Tabs } from "expo-router";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Octicons from "@expo/vector-icons/Octicons";
+export default function RootLayout() {
+  return (
+    <Tabs screenOptions={{headerShown:false}}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ size, color }) => (
+            <FontAwesome name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="units"
+        options={{
+          title: "Unit Converter",
+          tabBarIcon: ({ size, color }) => (
+            <Octicons name="arrow-switch" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="currency"
+        options={{
+          title: "Currency",
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="currency-exchange" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
